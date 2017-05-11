d3e88677514c9526911453745babf0a86b139ffa
Darby Losey <loseydm@cs.washington.edu>
2017-05-11 12:17:06 -0700

    Arduino interface complete and tested

---
+++ ArduinoInterface/Arduino2LightInterface.py
@@ -0,0 +1,95 @@
+import serial
+import time
+import CCDLUtil.Utility.AssertVal as CCDLAssert
+
+class Arduino2LightInterface(object):
+
+    NO_MSG = '0'
+    BOTH_ON_MSG = '1'
+    BOTH_OFF_MSG = '2'
+    LIGHT_3_ACTIVATE_MSG = '3'
+    LIGHT_4_ACTIVATE_MSG = '4'
+    LIGHT_3_DEACTIVATE_MSG = '5'
+    LIGHT_4_DEACTIVATE_MSG = '6'
+
+    VALID_MESSAGES = set(map(str, range(0, 7)))
+
+    def __init__(self, com_port, default_on=False, serial_init_post_delay=2.0, default_on_delay=0.0):
+        """
+        Initializes the arduino board to the given comport.  If default_on, a message
+        will be sent to the board that turns on both lights, else both lights start off.
+
+        This method takes serial_init_post_delay to create the serial port.
+        If default_on, this initialization will take an additional default_on_delay seconds.
+        :param com_port: The port to connect to
+        :param default_on: Will turn on lights if true and take an additional default_on_delay seconds
+                            to set up.
+        :param serial_init_post_delay: Time to wait after creating the serial port, defaults to 2
+        :param default_on_delay: Time to wait after sending the initial command to turn on the lights
+                                    defaults to 0.0.  Only relevant if default_on is True.
+        """
+
+        if type(com_port) is int:
+            com_port = 'COM%d' % com_port
+        self.ser = serial.Serial(com_port, 9600)
+        time.sleep(2.0)
+        if default_on:
+            self.write(self.LIGHT_3_DEACTIVATE_MSG)
+            time.sleep(2.0)
+
+    def write(self, msg, predelay=0.0, post_delay=0.0, run_post_check=False):
+        """
+        Sends a message to the board.  This method takes time post_delay to run.
+        If messages are sent too quickly, they may not be properly communicated to the board.
+        In practice this delay is about 2 seconds, which should be fine for most purposes.
+
+        No values are returned.
+
+        :param msg: msg must be an str(int) between 0 and 6. For a message key use the fields of the current class
+        :param predelay: Delay to wait prior to sending the message, defaults to zero
+        :param post_delay: Delay to wait post sending the message, defaults to zero
+        :param run_post_check: If True, will read a return message from the arduino confirming
+                                communication took place.  If it didn't, it will attempt to resend
+                                the message with a pre and post delay of 2 seconds.
+        """
+        def send_msg(to_send, delay1, delay2):
+            """
+            Sends the message with the given delays
+            """
+            time.sleep(delay1)
+            self.ser.write(to_send)
+            time.sleep(delay2)
+            return_msg = self.ser.read(self.ser.inWaiting())  # read all characters in buffer
+            return return_msg
+        if msg not in self.VALID_MESSAGES:
+            raise ValueError('The message must be valid')
+        msg = bytearray(msg, 'ascii')
+
+        ret_msg = send_msg(to_send=msg, delay1=predelay, delay2=post_delay)
+        if run_post_check and ret_msg != msg:
+            print msg, " - Arduino message not delivered with delays ", predelay, post_delay,
+            print "Retrying..."
+            ret_msg = send_msg(to_send=msg, delay1=2, delay2=2)
+            if ret_msg == msg:
+                print "Arduino Message delivered"
+            else:
+                raise RuntimeError('Arduino communication disrupted.')
+
+    def close_port(self):
+        """
+        Closes the Arduino serial port
+        """
+        self.ser.close()
+
+if __name__ == '__main__':
+    "Example Use Case"
+    ard = Arduino2LightInterface(com_port=10, default_on=True)
+    ard.write(Arduino2LightInterface.BOTH_ON_MSG)
+    time.sleep(2)
+    ard.write(Arduino2LightInterface.BOTH_OFF_MSG)
+    time.sleep(2)
+    ard.write(Arduino2LightInterface.BOTH_ON_MSG)
+    time.sleep(2)
+    ard.write(Arduino2LightInterface.BOTH_OFF_MSG)
+    time.sleep(2)
+    ard.close_port()
--- ArduinoInterface/SSVEP_2_LED/SSVEP_2_LED.ino
+++ ArduinoInterface/SSVEP_2_LED/SSVEP_2_LED.ino
@@ -1,33 +1,138 @@
+// Witten May 11, 2017 - Written to be used with the Arduino2LightInterface.py file in CCDLUtil
 
-int LED3 = 1;
+// "Pins 0 and 1 are used for serial communications. It's really not possible to use pins 0 and 1 for external circuitry 
+//    and still be able to utilize serial communications or to upload new sketches to the board."
 
+const int LED3 = 2;  // Shown on the case as slot 3
+const int LED4 = 3;  // // Shown on the case as slot 4
+
+// Messages we can send to the board
+const unsigned char NO_MSG = 0;
+const unsigned char BOTH_ON_MSG = 1;
+const unsigned char BOTH_OFF_MSG = 2;
+const unsigned char LIGHT_3_ACTIVATE_MSG = 3;
+const unsigned char LIGHT_4_ACTIVATE_MSG = 4;
+const unsigned char LIGHT_3_DEACTIVATE_MSG = 5;
+const unsigned char LIGHT_4_DEACTIVATE_MSG = 6;
+
+// 38 = Yes = 13 Hz
+// 42 = No = 12 Hz
 // 500 = 1 Hz for testing
-const unsigned long LED3_interval = 250; // Toggle every X milliseconds; Interval ON (and subsequently off) in ms.  For example setting to 500 will cause the light to blink at 1 Hz.
+const unsigned long LED3_interval = 29; // Interval between on and off in ms
+
+// 17 Hz = 29
+// 13 Hz = 38
+// 7 hz = 71
+// 5 hz = 100
+const unsigned long LED4_interval = 38; 
 
 unsigned long LED3_time;
+unsigned long LED4_time;
 unsigned char LED3_on;
+unsigned char LED4_on;
+
+unsigned char msg;  // 1 is to turn the lights on, 0 is to turn them off
+unsigned char light3_active;
+unsigned char light4_active;
 
 void setup () {
+  Serial.begin(9600);
   pinMode (LED3, OUTPUT);
+  pinMode (LED4, OUTPUT);
   LED3_time = millis ();
-  LED3_on = 0;
+  LED4_time = millis ();
+  LED3_on = 0;  // This flag is to denote whether the light is *currently* on
+  LED4_on = 0;
+  light3_active = 0; // This flag is to denote whether the light is currently blinking
+  light4_active = 0;
+  msg = 2;  // Default to all off
 }
+
+void turn_off_LED3 (){
+   if (LED3_on == 1) {
+      digitalWrite (LED3, LOW);
+      LED3_on = 0;
+   }
+}
+
+void turn_off_LED4 (){
+   if (LED4_on == 1) {
+      digitalWrite (LED4, LOW);
+      LED4_on = 0;  
+   }
+}
+
 
 void toggle_LED3 () {
    if (LED3_on == 0) {
-      digitalWrite (LED3, LOW);
+      digitalWrite (LED3, HIGH);
       LED3_on = 1;
    }
    else {
       digitalWrite (LED3, LOW);
       LED3_on = 0;
    }
-  LED3_time = millis ();  
-}
 
-void loop ()
-  {
-    if ((millis () - LED3_time) >= LED3_interval) {
-      toggle_LED3 ();
+  LED3_time = millis ();  
+  }
+  
+ void toggle_LED4 (){
+   if (LED4_on == 0) {
+      digitalWrite (LED4, HIGH);
+      LED4_on = 1;
+   }
+   else {
+      digitalWrite (LED4, LOW);
+      LED4_on = 0;
+   }
+
+  LED4_time = millis ();  
+  }
+
+void loop () {
+  while (Serial.available() > 0){
+    msg = Serial.read();
+    delay(200);
+    msg = msg - 48;
+    Serial.print(msg);
+  }
+  
+  if (msg == BOTH_ON_MSG) {
+    // If we get the high both message and either of our lights are off
+    light3_active = 1;
+    light4_active = 1;
+  } else if (msg == BOTH_OFF_MSG) {
+    // If we get the low both message, we need to set our flags *and* turn off both lights
+    light3_active = 0;
+    light4_active = 0;
+    turn_off_LED3();
+    turn_off_LED4();
+  } else if (msg == LIGHT_3_ACTIVATE_MSG) {
+    // If we get the message high 3, we need to activate light 3
+    light3_active = 1;
+  } else if (msg == LIGHT_4_ACTIVATE_MSG) {
+    // If we get the message high 3, we need to activate light 3
+    light4_active = 1;
+  } else if (msg == LIGHT_3_DEACTIVATE_MSG) {
+    // If we get the message low 3, we need to turn off light 3 and deactivate light 3
+    light3_active = 0;
+    turn_off_LED3();
+  } else if (msg == LIGHT_4_DEACTIVATE_MSG) {
+    // If we get the message low 4, we need to turn off light 4 and deactivate
+    light4_active = 0;
+    turn_off_LED4();
+  }
+  msg = NO_MSG;  // Reset our message so we don't continually go into the if/else branches
+
+  // Toggle the lights
+  if (light3_active == 1) {
+    if (millis () - LED3_time >= LED3_interval) {
+      toggle_LED3();
     }
+  }
+  if (light4_active == 1) {
+    if (millis () - LED4_time >= LED4_interval) {
+      toggle_LED4();
+    }
+  }
 }

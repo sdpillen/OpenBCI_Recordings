a8b25695d3ca0c2885725ba0ea54c8dd27d9d607
Rogue <nimbalo61@gmail.com>
2017-06-07 16:39:18 -0700

    update

--- Communications/CheckMultiNetworksSend.py
+++ Communications/CheckMultiNetworksSend.py
@@ -13,13 +13,13 @@ if __name__ == '__main__':
 
     RECEIVE_MESSAGE_QUEUE = Queue.Queue()
     SEND_MESSAGE_QUEUE = Queue.Queue()
-    RECEIVE_OBJECT = Receive.Receive(port=13000, receive_message_queue=RECEIVE_MESSAGE_QUEUE)
+    RECEIVE_OBJECT = Receive.Receive(port=13001, receive_message_queue=RECEIVE_MESSAGE_QUEUE)
     SEND_OBJECT = Send.Send(host_ip=PERSONAL_COMPUTER_IP, port=13000, send_message_queue=SEND_MESSAGE_QUEUE)
     threading.Thread(target=RECEIVE_OBJECT.receive_from_queue).start()
     threading.Thread(target=SEND_OBJECT.run_send_from_queue).start()
 
     while True:
         data = raw_input("Enter message to send or type 'exit': ")
-        SEND_MESSAGE_QUEUE.put(data + '-- Resent')
+        SEND_MESSAGE_QUEUE.put(data)
         data = RECEIVE_MESSAGE_QUEUE.get()
         print "Received message: " + data

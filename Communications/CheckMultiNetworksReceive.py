668467151481c1ba27b93ad46e4a4d6da7425a7a
Darby Losey <loseydm@cs.washington.edu>
2017-06-07 16:38:30 -0700

    update

--- Communications/CheckMultiNetworksReceive.py
+++ Communications/CheckMultiNetworksReceive.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     RECEIVE_MESSAGE_QUEUE = Queue.Queue()
     SEND_MESSAGE_QUEUE = Queue.Queue()
     RECEIVE_OBJECT = Receive.Receive(port=13000, receive_message_queue=RECEIVE_MESSAGE_QUEUE)
-    SEND_OBJECT = Send.Send(host_ip=CSE208_IP, port=13000, send_message_queue=SEND_MESSAGE_QUEUE)
+    SEND_OBJECT = Send.Send(host_ip=CSE208_IP, port=13001, send_message_queue=SEND_MESSAGE_QUEUE)
     threading.Thread(target=RECEIVE_OBJECT.receive_from_queue).start()
     threading.Thread(target=SEND_OBJECT.run_send_from_queue).start()
 

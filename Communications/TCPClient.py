63bf4af54632720ee2f9010a7304e3b9cf70c674
Preston Jiang <prestonj@cs.washington.edu>
2017-06-26 17:11:20 -0700

    update TCP communication model and docs

--- Communications/TCPClient.py
+++ Communications/TCPClient.py
@@ -1,18 +1,22 @@
 """
-This method is for sending messages between computers on the same network.
+This class represents a TCP Client which can receive and send messages to a TCP Server
 
-Messages are sent as strings.  If sending a nonstring object, it will be converted
-to a string.
+If connection is unsuccessful, be sure to check the firewall settings (especially if Mcafee is installed, check if
+public network connection is disabled)
 """
-import os
 import socket
-import threading
-import Queue
 
 
 class TCPClient(object):
 
     def __init__(self, server_ip, port, receive_message_queue, send_message_queue, buf=1024):
+        """Initialize a TCP Client object
+        :param server_ip: the ip address of the server to connect to
+        :param port: port number
+        :param receive_message_queue: the queue to receive messages from (server)
+        :param send_message_queue: the queue to sendn
+        :param buf: the buffer size to send/receive messages (default to 1024 bytes)
+        """
         self.buf = buf
         self.ip = server_ip
         self.port = port
@@ -24,41 +28,19 @@ class TCPClient(object):
         self.receive_message_queue = receive_message_queue
 
     def start_receive_from_queue(self):
+        """start receiving messages from server and pass them to the receive_message_queue
+        :return: none
+        """
         while True:
             received_message = self.TCPSock.recv(self.buf)
             print "Server sends: " + received_message
             self.receive_message_queue.put(received_message)
 
     def start_send_to_queue(self):
-        """
-        Reads messages from the send_messages_queue passed during initialization
-
-        Sends data in accordance with the host and port set during initialization
-
-        Pass 'exit' to close the socket.
-
-        Data put on queue will be cast to a string!
+        """start sending messages to server and pass them to the send_message_queue. Send 'exit' to close the socket
+        :return: none
         """
         while True:
             message_to_send = str(self.send_message_queue.get())
             print "Sending", message_to_send
             self.TCPSock.send(message_to_send)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- Communications/TCPServer.py
+++ Communications/TCPServer.py
@@ -1,13 +1,20 @@
+"""
+This class represents a TCP server which can send and receive messages from a TCP Client
+
+If connection is unsuccessful, be sure to check the firewall settings (especially if Mcafee is installed, check if
+public network connection is disabled)
+"""
 import socket
+
 
 class TCPServer(object):
 
-    def __init__(self, port, receive_message_queue, send_message_queue, host="", buf=1024):
-        """
-        Sends messages to the specified host and port for computers on the same network.
-        :param host: Defaults to ""
-        :param port: Set to desired port. Only one Send object can be opened on a given port. Example: 13000
-        :param send_message_queue: Place messages on this queue to have them sent.
+    def __init__(self, port, receive_message_queue, send_message_queue, buf=1024):
+        """Initialize a TCP Server object and wait for clients to connect
+        :param port: port number
+        :param receive_message_queue: the queue to receive messages from (client)
+        :param send_message_queue: the queue to send messages to (client)
+        :param buf: the buffer size to send/receive messages (default to 1024 bytes)
         """
         self.port = port
         self.buf = buf
@@ -22,15 +29,9 @@ class TCPServer(object):
         self.send_message_queue = send_message_queue
 
     def start_receive_from_queue(self):
-        """
-        Receives messages and passes them to the receive_message_queue passed during initialization
-    
-        Received data in accordance with the host and port set during initialization
-    
-        Pass 'exit' to close the socket.
-    
-        All data passed is of string format -- meaning all data placed on the receive_message_queue will
-        be a string.
+        """Receive messages from clients and pass them to the receive_message_queue. Close the socket if the message
+        received is 'exit'
+        :return: none
         """
         while True:
             message = str(self.conn.recv(self.buf))
@@ -40,7 +41,10 @@ class TCPServer(object):
                 self.TCPSock.close()
 
     def start_send_to_queue(self):
+        """Send messages to clients and pass them to the send_message_queue.
+        :return: none
+        """
         while True:
-            message_to_send = self.send_message_queue.get()  # blocking call
+            message_to_send = self.send_message_queue.get()
             print "Sending... ", message_to_send
             self.conn.sendall(message_to_send)

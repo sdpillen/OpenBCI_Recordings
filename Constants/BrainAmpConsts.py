7efd5ba6826e770c59a2aefd818bc98d084e17c0
Linxing Preston Jiang <prestonj@cs.washington.edu>
2017-07-31 15:05:48 -0700

    add header size to constants file

--- Constants/BrainAmpConsts.py
+++ Constants/BrainAmpConsts.py
@@ -21,6 +21,8 @@ START_KEYS= ['start_left_time', 'start_rest2_time', 'start_right_time', 'start_r
 END_KEYS = ['end_left_time', 'end_rest2_time', 'end_right_time', 'end_rest1_time']
 # the column index in csv file that stores time stamps
 TIME_INDEX = 1
+# header size
+HEADER_SIZE = 9
 # the column index of the first channel in csv file
 DATA_INDEX_START = 2
 # the column index of the last channel in csv file

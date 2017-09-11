5976bca3e453be9663d540fd6bf2bae89f485034
Linxing Preston Jiang <prestonj@cs.washington.edu>
2017-09-11 15:09:44 -0700

    change EEG interface design

--- EEGInterface/EEGInterfaceParent.py
+++ EEGInterface/EEGInterfaceParent.py
@@ -1,12 +1,10 @@
-import threading
-import EEGDataSaver
-import Queue
-
 """
 To save data, it is suggested that this module is used with the EEGDataSaver.
 threading.Thread(target=lambda: EEGDataSaver.start_eeg_data_saving(subject_data_path, self.data_save_queue, header=None)).start()
 
 """
+
+import Queue
 
 
 class EEGInterfaceParent(object):
@@ -15,26 +13,17 @@ class EEGInterfaceParent(object):
     A Parent interface that should be inherited other systems that interface with EEG.
     """
 
-    def __init__(self, out_buffer_queue, channels_for_live='All', put_data_on_out_queue_flag=False, data_save_queue=None, subject_name=None, subject_tracking_number=None, experiment_number=None):
+    def __init__(self, channels_for_live='All', live=True, save_data=True, subject_name=None,
+                 subject_tracking_number=None, experiment_number=None):
         """
-        A data collection object for the EEG interface.  This provides option for live data streaming and saving data to file.
-
-        For live data streaming, use with a threading or multiprocessing queue (ie. Queue.queue()
-           Data will be put on the queue, which can be read by another thread.)
+        A data collection object for the EEG interface.
+        This provides option for live data streaming and saving data to file.
 
         Modifies the EEG_INDEX and EEG_INDEX_2 in CCDLUtil/EEGInterface/EEG_INDEX.py when each packet arrives.  These variables can be read from any thread.
             Use this to time mark events in your other programs.
 
         :param channels_for_live: List of channel names (or indexes) to put on the out_buffer_queue. If [], no channels will be put on the out_buffer_queue.
                                   If 'All' (case is ignored), all channels will be placed on the out_buffer_queue.  Defaults to All.
-        
-        :param put_data_on_out_queue_flag: We will only put data on our out queue if this flag is true.  This can be done so data streaming can be turned on and off externally.
-             
-        :param data_save_queue: queue to put data to save.  If None, data will not be saved.
-        
-        :param out_buffer_queue: The channel listed in the channels_for_live parameter will be placed on this queue. This is intended for live data analysis.
-                                 If None, no data will be put on the queue.
-                                 Items put on the out buffer queue will be a numpy array (though this can be either a 2D or a 1D numpy array)
         
         :param subject_name: Optional -- Name of the subject. Defaults to 'None'
         :param subject_tracking_number: Optional -- Subject Tracking Number (AKA TMS group experiment number tracker). Defaults to 'None'
@@ -44,24 +33,21 @@ class EEGInterfaceParent(object):
         self.subject_name = str(subject_name) if subject_name is not None else "None"
         self.subject_number = str(subject_tracking_number) if subject_tracking_number is not None else 'None'
         self.experiment_number = str(experiment_number) if experiment_number is not None else 'None'
-
         # Count each block we receive, start at -1 to avoid a fence/post problem.
         self.data_index = -1
-
+        # data saving and live analysis flags
+        self.live = live
+        self.save_data = save_data
         # A separate queue (other than the one for storing data) that puts the channels_for_live data points on
-        self.out_buffer_queue = out_buffer_queue
-
+        self.out_buffer_queue = Queue.Queue() if live else None
         # block counter to check overflows of tcpip buffer
         self.last_block = -1
-
-        self.put_data_on_out_queue_flag = put_data_on_out_queue_flag
         self.channels_for_live = channels_for_live
         if type(self.channels_for_live) is str:
             self.channels_for_live = self.channels_for_live.lower()
             if self.channels_for_live != 'all':
                 raise ValueError('Invalid channels_for_live parameter')
-        self.data_save_queue = data_save_queue
-
+        self.data_save_queue = Queue.Queue if save_data else None
 
     def trim_channels_with_channel_index_list(self, data, channel_index_list):
         """
--- EEGInterface/OpenBCI/OpenBCIStreamer.py
+++ EEGInterface/OpenBCI/OpenBCIStreamer.py
@@ -5,13 +5,15 @@ import CCDLUtil.EEGInterface.EEGInterfaceParent
 import Queue
 import threading
 import CCDLUtil.EEGInterface.EEGDataSaver as EEGDataSaver
+from CCDLUtil.Utility.Decorators import threaded
 import time
 
 
 class OpenBCIStreamer(CCDLUtil.EEGInterface.EEGInterfaceParent.EEGInterfaceParent):
 
-    def __init__(self, out_buffer_queue, channels_for_live='All', channels_for_save='All', include_aux_in_save_file=True, data_save_queue=None, subject_name=None,
-                 subject_tracking_number=None, experiment_number=None, channel_names=None, port=None, baud=115200):
+    def __init__(self, channels_for_live='All', channels_for_save='All', live=True, save_data=True,
+                 include_aux_in_save_file=True, subject_name=None, subject_tracking_number=None, experiment_number=None,
+                 channel_names=None, port=None, baud=115200):
         """
         A data collection object for the EEG interface.  This provides option for live data streaming and saving data to file.
 
@@ -37,18 +39,16 @@ class OpenBCIStreamer(CCDLUtil.EEGInterface.EEGInterfaceParent.EEGInterfaceParen
         :param experiment_number: Optional -- Experimental number. Defaults to 'None'
         """
 
-        super(OpenBCIStreamer, self).__init__(channels_for_live, out_buffer_queue, data_save_queue=data_save_queue, subject_name=subject_name,
-                                              subject_tracking_number=subject_tracking_number, experiment_number=experiment_number)
+        super(OpenBCIStreamer, self).__init__(
+            channels_for_live=channels_for_live, live=live, save_data=save_data, subject_name=subject_name,
+            subject_tracking_number=subject_tracking_number, experiment_number=experiment_number)
         # in super, self.data_index is set to 0
         self.channel_names = str(channel_names)
         self.channels_for_save = channels_for_save
         self.include_aux_in_save_file = include_aux_in_save_file
         self.channels_for_live = channels_for_live
-
         # Set our port to default if a port isn't passed
         self.port = OpenBCIStreamer.get_default_port() if port is None else port
-
-        self.out_buffer_queue = out_buffer_queue
         self.baud = baud
 
     @staticmethod
@@ -64,27 +64,31 @@ class OpenBCIStreamer(CCDLUtil.EEGInterface.EEGInterfaceParent.EEGInterfaceParen
         else:
             raise ValueError('Default Port for OS not recognized')
 
-    def write_file_callback(self, data_packet):
+    def callback_fn(self, data_packet):
         """
         This is a callback from our OpenBCI board.  A data packet is passed (a list of the data points)
+
         :param data_packet:  Data as taken from the OpenBCI board.  This is generally an OpenBCI sample object.
         """
-
         try:
-            data = data_packet.channel_data  #  List of data points
-            self.data_index += 1     # data_index starts at -1, thus our data is always indexed >= 0 by incrementing here instead of at the end of the method.
-            aux_data = data_packet.aux_data  # List of AUX data (note that this is sampled at a fraction of the rate of the data.  Each packet will have this field
-                                             # but packets that don't have aux data will have a list of zeros.
+            # List of data points
+            data = data_packet.channel_data
+            # starts at -1, our data is always indexed >= 0 by incrementing here instead of at the end of the method.
+            self.data_index += 1
+            # List of AUX data. note that this is sampled at a fraction of the rate of the data.
+            # Each packet will have this field
+            aux_data = data_packet.aux_data
+            # but packets that don't have aux data will have a list of zeros.
             id_val = data_packet.id
-
 
         except Exception as e:
             # If we throw an error in this portion of the code, exit everything
             print e.message, e
-            raise  e # Won't run
+            # Won't run
+            raise e
 
         # Put on Out Buffer for live data analysis.
-        if self.out_buffer_queue is not None:
+        if self.live:
             # Get our channels from channels for live:
             if type(self.channels_for_live) is str and self.channels_for_live.lower() == 'all':
                 self.out_buffer_queue.put(data)
@@ -94,10 +98,9 @@ class OpenBCIStreamer(CCDLUtil.EEGInterface.EEGInterfaceParent.EEGInterfaceParen
                 self.out_buffer_queue.put(trimmed_data)
             else:
                 if self.channels_for_live is not None:
-
                     raise ValueError('Invalid channels_for_live value.')
-
-        if self.data_save_queue is not None:
+        # Save data
+        if self.save_data:
             data_to_put_on_queue = None
             if type(self.channels_for_save) is str and self.channels_for_save.lower() == 'all':
                 data_to_put_on_queue = data
@@ -109,7 +112,7 @@ class OpenBCIStreamer(CCDLUtil.EEGInterface.EEGInterfaceParent.EEGInterfaceParen
 
             if data_to_put_on_queue is not None:
 
-                data_str = str(id_val) + ',' + str(time.time()) + ',' + ','.join([str(xx) for xx in data_to_put_on_queue])
+                data_str = str(id_val)+','+str(time.time())+','+','.join([str(xx) for xx in data_to_put_on_queue])
 
                 if self.include_aux_in_save_file:
                     data_str += ',' + ','.join([str(yy) for yy in aux_data])
@@ -129,8 +132,9 @@ class OpenBCIStreamer(CCDLUtil.EEGInterface.EEGInterfaceParent.EEGInterfaceParen
         To keep naming consistant across EEG systems, calling .start_recording provides the same utility.
         """
         board = BciHwInter.OpenBCIBoard(port=self.port, baud=self.baud, scaled_output=False, log=True)
-        board.start_streaming(self.write_file_callback)
+        board.start_streaming(self.callback_fn)
 
+    @threaded
     def start_recording(self):
         """
         Starts the open BciHwInter streamer. This method streams data infinitely and does not return.
@@ -138,8 +142,9 @@ class OpenBCIStreamer(CCDLUtil.EEGInterface.EEGInterfaceParent.EEGInterfaceParen
         """
         self.start_open_bci_streamer()
 
+
 if __name__ == '__main__':
-    data_save_queue = Queue.Queue()
-    obs = OpenBCIStreamer(out_buffer_queue=None, data_save_queue=data_save_queue, port="COM5")
-    threading.Thread(target=lambda: EEGDataSaver.start_eeg_data_saving(save_data_file_path='RestingStateMay24.csv', queue=data_save_queue, header="Sample Header")).start()
-    obs.start_open_bci_streamer()
+    obs = OpenBCIStreamer(live=True, save_data=True, port='/dev/ttyUSB0/')
+    obs.start_recording()
+    threading.Thread(target=lambda: EEGDataSaver.start_eeg_data_saving(save_data_file_path='RestingStateMay24.csv',
+                                                                       queue=obs.data_save_queue, header="Sample Header")).start()

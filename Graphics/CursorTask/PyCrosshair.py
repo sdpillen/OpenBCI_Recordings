620f6773752c0a7d9897b4c4aab2e3e7f3b7c0e3
Linxing Preston Jiang <prestonj@cs.washington.edu>
2017-09-23 22:24:31 -0700

    update graphics to new threated decorator

--- Graphics/CursorTask/PyCrosshair.py
+++ Graphics/CursorTask/PyCrosshair.py
@@ -189,7 +189,7 @@ class PyCrosshair(object):
         """
         pygame.event.clear()
 
-    @threaded
+    @threaded(True)
     def _run_with_queue(self):
         """
         Runs our game by reading off events off the passed queue. Events should be passed in the form:
